4148e50f2e513f4463297deeb1a1d04420eab608
Juanma <elwe.isilra@gmail.com>
2022-11-18 13:54:57 +0000

    Hasta 59

---
+++ src/app/pages/list/list.page.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonList } from '@ionic/angular';
+import { Observable } from 'rxjs';
+import { DataService } from 'src/app/services/data.service';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.page.html',
+  styleUrls: ['./list.page.scss'],
+})
+export class ListPage implements OnInit {
+
+  usuarios: Observable<any> | undefined;
+
+  @ViewChild (IonList, {static: false}) ionlist:IonList;
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit() {
+
+    this.usuarios = this.dataService.getUsuarios();
+  }
+
+  favorite(user:any) {
+
+    console.log(user);
+    this.ionlist.closeSlidingItems;
+  }
+
+  share(user:any){
+
+ console.log(user);
+ this.ionlist.closeSlidingItems;
+  }
+
+  delete(user:any){
+    console.log(user);
+    this.ionlist.closeSlidingItems;
+  }
+
+  
+}
